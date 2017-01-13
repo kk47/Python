5fc8a44ae1959bddf600f8e52e7f5a745deae041
kk47 <kk47yx@gmail.com>
2017-01-13 10:53:05 +0800

    - add stuff

--- test/threading_test.py
+++ test/threading_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import threading
 import time
 import subprocess
@@ -20,16 +21,28 @@ def fun(a1):
 
 if __name__ == "__main__":
     
-    a1 = "kk"
+    '''a1 = "kk"
     
     p = multiprocessing.Process(target=fun, args=("a1",))
     p.daemon = True
     p.start()
     
-    #subproc = subprocess.Popen([fun,a1], stderr=subprocess.PIPE)
-    #print subproc.pid
-    '''
     t = threading.Thread(target=fun, args=(a1))
     t.setDaemon(True)
     t.start()
     #t.join()'''
+
+    sub1 = subprocess.Popen('./prog1.sh'.split(), stdout=subprocess.PIPE)
+    sub2 = subprocess.Popen('./prog2.sh'.split(), stdout=subprocess.PIPE)
+
+    sublist = [sub1, sub2]
+    
+    com_list = []
+    while len(com_list) != 2:
+        for sub in sublist:
+            out = sub.stdout.readline()
+            if not out and sub.poll() != None:
+                if sub not in com_list:
+                    com_list.append(sub)
+            if out:
+                sys.stdout.write(out)
