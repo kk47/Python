f94f888bf6089f757cc14a41de8db11a05b58c03
kk47 <kk47yx@gmail.com>
2017-03-20 17:30:16 +0800

    add decorator test

---
+++ test/decorator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env
+import time
+import logging
+
+class Test(object):
+
+    def _profiler(func):
+        def func_wrapper(self, *args, **kwargs):
+            start_time = time.time()
+            res = func(self, *args, **kwargs)
+            logging.warning("Function %s spend %s", func.__name__, time.time() - start_time)
+            return res
+        return func_wrapper
+    
+    @_profiler
+    def foo(self, arg1, arg2):
+        msg = "A message, %s %s" % (arg1, arg2)
+        time.sleep(2)
+        return True, msg 
+
+    @_profiler
+    def bar(self, arg1):
+        msg = "A message, %s" % arg1
+        time.sleep(3)
+        return True, msg 
+
+if __name__ == "__main__":
+    
+    t = Test()
+    print t.foo("Lucky", 'boy')
+    print t.bar('Handsome')
